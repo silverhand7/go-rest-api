f26a21798cf9623b18404cbc1e87cb026eaaf900
silverhand7 <refojunior5@gmail.com>
2023-09-06 09:36:42 +0800

    error handler

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -3,12 +3,17 @@ package exception
 import (
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/go-rest-api/helpers"
 	"github.com/go-rest-api/model/web"
 )
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request, err interface{}) {
 	if notFoundError(w, r, err) {
+		return
+	}
+
+	if validationErrors(w, r, err) {
 		return
 	}
 
@@ -25,6 +30,25 @@ func notFoundError(w http.ResponseWriter, r *http.Request, err interface{}) bool
 			Code:   http.StatusNotFound,
 			Status: "Not Found",
 			Data:   exception.Error,
+		}
+
+		helpers.WriteToResponseBody(w, webResponse)
+		return true
+	} else {
+		return false
+	}
+}
+
+func validationErrors(w http.ResponseWriter, r *http.Request, err interface{}) bool {
+	exception, ok := err.(validator.ValidationErrors)
+	if ok {
+		w.Header().Set("X-Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusBadRequest,
+			Status: "Bad Request",
+			Data:   exception.Error(),
 		}
 
 		helpers.WriteToResponseBody(w, webResponse)
